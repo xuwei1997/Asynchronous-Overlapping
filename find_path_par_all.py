07e395cfcaaed997c51e523105b5f2c5fdf50242
xuwei <670525744@qq.com>
2021-07-01 20:12:12 +0800

    0701

--- find_path_par_all.py
+++ find_path_par_all.py
@@ -9,6 +9,7 @@ from sko.PSO import PSO
 from sko.tools import set_run_mode
 import matplotlib.pyplot as plt
 from pathos.multiprocessing import ProcessingPool as Pool
+# from pathos.multiprocessing import Pool
 # from multiprocessing import Pool
 # from multiprocessing.dummy import Pool as ThreadPool
 import os
@@ -23,9 +24,9 @@ def out_log(string):
 def find_path_one_img(img_PATH,COOL, laplace_w, direction_w, magnitude_w):
     img_G, img_RGB=img_PATH
 
-    img_G_path = os.path.join(r'/tmp/pycharm_project_836/gary1', img_G)
-    img_RGB_path = os.path.join(r'/tmp/pycharm_project_836/rbg1', img_RGB)
-    img_mark_path = os.path.join(r'/tmp/pycharm_project_836/mark_or', img_RGB)
+    img_G_path = os.path.join(r'/home/zhny/pycharm01/gary1', img_G)
+    img_RGB_path = os.path.join(r'/home/zhny/pycharm01/rbg1', img_RGB)
+    img_mark_path = os.path.join(r'/home/zhny/pycharm01/mark_or', img_RGB)
 
     # print(img_G_path, img_RGB_path)
 
@@ -40,7 +41,7 @@ def find_path_one_img(img_PATH,COOL, laplace_w, direction_w, magnitude_w):
     # scissors_list = []
     # cool_list = []
     for k in contours_g:
-        if len(k) > 150:
+        if len(k) > 50:#50!
             # print(len(k))
             contours_g_out.append(k)
             # scissors_list.append(scissors)
@@ -52,8 +53,12 @@ def find_path_one_img(img_PATH,COOL, laplace_w, direction_w, magnitude_w):
     # #  单线进程方法2！！
     Intelligent_scissors_par = partial(Intelligent_scissors, scissors=scissors, cool_number=COOL)
     # print('map')
-    out_end = list(map(Intelligent_scissors_par, contours_g_out))
+    # out_end = list(map(Intelligent_scissors_par, contours_g_out))
     # print(out_end)
+    #多进程方法
+    # print('map')
+    p2 = Pool()
+    out_end = p2.map(Intelligent_scissors_par, contours_g_out)
 
     # 画边缘
     img_out = cv2.drawContours(img_rgb_c, out_end, -1, (255, 0, 0), 3)
@@ -65,7 +70,7 @@ def find_path_one_img(img_PATH,COOL, laplace_w, direction_w, magnitude_w):
     #处理路径并保存
     path_mask = str(COOL) + '_' + str(laplace_w) + '_' + str(direction_w) + '_' + str(magnitude_w)
     # path_out = str(COOL) + '_' + str(laplace_w) + '_' + str(direction_w) + '_' + str(magnitude_w)
-    mask_dire=os.path.join(r'/tmp/pycharm_project_836/',path_mask)
+    mask_dire=os.path.join(r'/home/zhny/pycharm01/',path_mask)
     out_dire=os.path.join(mask_dire,'out_rgb/')
     if os.path.exists(mask_dire) == False:  # 查看是否有文件夹
         os.mkdir(mask_dire)
@@ -88,8 +93,8 @@ def find_path_func(X):  # COOL缩放过
     print(COOL, laplace_w, direction_w, magnitude_w)
     COOL = int(COOL * 100)
 
-    path_gary = r'/tmp/pycharm_project_836/gary1'
-    path_rgb = r'/tmp/pycharm_project_836/rbg1'
+    path_gary = r'/home/zhny/pycharm01/gary1'
+    path_rgb = r'/home/zhny/pycharm01/rbg1'
 
     # 按文件名排列
     f_gary = os.listdir(path_gary)
@@ -98,6 +103,8 @@ def find_path_func(X):  # COOL缩放过
     f_rgb = sorted(f_rgb)
     f_zip=zip(f_gary,f_rgb)
 
+
+    #找边缘
     find_path_one_img_par=partial(find_path_one_img,COOL=COOL, laplace_w=laplace_w, direction_w=direction_w, magnitude_w=magnitude_w)
     niou=list(map(find_path_one_img_par,f_zip))
     print("niou")
@@ -115,6 +122,8 @@ if __name__ == '__main__':
     # print(iou)
 
     set_run_mode(find_path_func, 'multiprocessing')
+    # set_run_mode(find_path_func, 'multithreading')
+
     print("pao1")
     pso = PSO(func=find_path_func, n_dim=4, pop=15, max_iter=30, lb=[0.1, 0.1, 0.1, 0.1], ub=[0.28, 0.8, 0.8, 0.8],
               w=0.8, c1=0.5, c2=0.5)
@@ -122,9 +131,20 @@ if __name__ == '__main__':
     pso.record_mode = True
     print("pao3")
 
-    #5
+    # 1
     print("pao4")
-    pso.run(5)
+    pso.run(2)
+    print('best_x is ', pso.gbest_x, 'best_y is', pso.gbest_y)
+    plt.plot(pso.gbest_y_hist)
+    plt.savefig('2.png')
+    record_dict = pso.record_value
+    f = open('2.txt', 'w')
+    f.write(str(record_dict) + '\n' + 'best_x is ' + str(pso.gbest_x) + 'best_y is' + str(pso.gbest_y))
+    f.close()
+
+    5
+    print("pao4")
+    pso.run(4)
     print('best_x is ', pso.gbest_x, 'best_y is', pso.gbest_y)
     plt.plot(pso.gbest_y_hist)
     plt.savefig('5.png')
@@ -164,4 +184,3 @@ if __name__ == '__main__':
     f.close()
 
     print(pso.record_value)
-
